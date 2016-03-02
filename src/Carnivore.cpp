d474df198abcc878fdacb02633e04ff0a2d6d4b9
rmxhaha <code000123@gmail.com>
2016-03-02 21:43:34 +0700

    adding basic carnivore

---
+++ src/Carnivore.cpp
@@ -0,0 +1,4 @@
+#include "Carnivore.hpp"
+
+Carnivore::Carnivore(Board& b, int x, int y, int t_lahir) : Animal(b,x,y,t_lahir) {
+}
---
+++ src/Carnivore.hpp
@@ -0,0 +1,9 @@
+#include "Animal.hpp"
+
+class Carnivore : public Animal {
+public:
+    Carnivore(Board& b, int x, int y, int t_lahir);
+
+protected:
+
+};
