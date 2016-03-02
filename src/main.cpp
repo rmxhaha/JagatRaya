794d7e722cc04d0e494f2add67802a2c05177423
rmxhaha <code000123@gmail.com>
2016-03-02 23:14:54 +0700

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "IsA.hpp"
-
+#include "Universe.hpp"
+#include "Human.hpp"
 #include <iostream>
 using namespace std;
 
 int main(){
-	cout << "testing" << endl;
-	
+	Universe u(10,10);
+    u.add(new Human(u,0,0,10));
     return 0;
 }
 
