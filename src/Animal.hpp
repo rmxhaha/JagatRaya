35cd332aa78503d84f73dd412c1032cdc9c11f84
rmxhaha <code000123@gmail.com>
2016-03-02 21:39:01 +0700

    goTo and avoid becomes protected

--- src/Animal.hpp
+++ src/Animal.hpp
@@ -11,9 +11,9 @@ public:
 	Animal(Board& b, int x, int y, int t_lahir);
 	virtual float speed() const = 0;
 
+protected:
 	direction_t avoid(int x, int y);
 	direction_t goTo(int x, int y);
-protected:
 	void move(direction_t direction);
 
 };
