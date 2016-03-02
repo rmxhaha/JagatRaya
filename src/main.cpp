e1dfb19ef9f8539c626edfc4e28163611573ba52
rmxhaha <code000123@gmail.com>
2016-03-02 23:19:42 +0700

    timer for update temporary

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include "IsA.hpp"
 #include "Universe.hpp"
 #include "Human.hpp"
+#include "windows.h"
+
 #include <iostream>
 using namespace std;
 
 int main(){
 	Universe u(10,10);
-    u.add(new Human(u,0,0,10));
+	Human* h = new Human(u,0,0,10);
+    u.add(h);
+    while(true){
+        u.board.PrintBoard();
+        h->update(1);
+        Sleep(1000);
+    }
     return 0;
 }
 
