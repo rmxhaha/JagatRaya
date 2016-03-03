c9219c4ca571a6617dcaef76a4234c6c17df25b2
rmxhaha <code000123@gmail.com>
2016-03-03 07:53:08 +0700

    adding tiger to the world

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Universe.hpp"
 #include "Human.hpp"
 #include "windows.h"
+#include "Tiger.hpp"
 
 #include <iostream>
 using namespace std;
@@ -9,11 +10,15 @@ using namespace std;
 int main(){
 	Universe u(10,10);
 	Human* h = new Human(u,0,0,10);
+	Tiger* t = new Tiger(u,0,1,9);
+
     u.add(h);
+    u.add(t);
+
     while(true){
         u.board.PrintBoard();
-        u.update(1);
-        Sleep(1000);
+        u.update(100);
+        Sleep(100);
     }
     return 0;
 }
