0eb6b3b14c7a34384b0cdeeba37df9b12f10c64b
rmxhaha <code000123@gmail.com>
2016-03-03 07:52:15 +0700

    adding move randomly on tiger

--- src/Tiger.cpp
+++ src/Tiger.cpp
@@ -4,6 +4,7 @@ Tiger::Tiger(Universe& u, int x, int y, int t_lahir):Carnivore(u,x,y,t_lahir){}
 
 void Tiger::update_logic(){
 	//Update Tiger move
+	move(goRandom());
 }
 char Tiger::ch() const {
 	return 'T';
