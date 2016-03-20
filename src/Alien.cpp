5bbe3d8b0c57db8deb5b569c311972ca39b2bd80
ab <alif_bhaskoro@yahoo.com>
2016-03-20 21:48:04 +0700

    Alien jalannya aneh cma uda bisa bnuh dan ngejer mangsa tp lama

--- src/Alien.cpp
+++ src/Alien.cpp
@@ -2,10 +2,71 @@
 
 Alien::Alien(Universe& u, int x, int y,float currentAge):Carnivore(u,x,y,currentAge){}
 
-void Alien::update(){//Update Alien move
+void Alien::update_logic(){//Update Alien move
+	int prey_x;
+	int prey_y;
+	move(goRandom());
+	if(!find(prey_x,prey_y,x,y)){
+		move(goRandom());
+	}
+	else{
+		move(goTo(prey_x,prey_y));
+	}
 }
 
+void Alien::interact(Organism * O){
+	O->forceKill();
+}
 char Alien::ch() const{return '@';}
-float Alien::speed() const{return 1;}
+float Alien::speed() const{return 3;}
 int Alien::power() const{return 50;}
-int Alien::age() const{return 100;}
+int Alien::age() const{return 100000;}
+
+class Target {
+    public:
+	Target(){
+    	x = 0;
+    	y = 0;
+    	distance = 0;
+	}
+	int x,y,distance;
+};
+
+bool Alien::find(int & prey_x,int & prey_y,int predator_x,int predator_y){
+	vector<Target> vec;
+	Board& board = universe->board;
+	int i = 0;
+	int j;
+	Target T;
+	
+	while(i<board.GetH()){
+		j=0;
+		while(j<board.GetW()){
+			if(board.GetEl(i,j).length()>0){
+				T.x = i;
+				T.y = j;
+				T.distance = (predator_x-T.x)*(predator_x-T.x) + (predator_y-T.y)*(predator_y-T.y);
+				vec.push_back(T);
+			}
+			j++;
+		}
+		i++;
+	}
+	i=1;
+	if(vec.size()>0){
+		int min = vec[0].distance;
+		prey_x=vec[0].x;
+		prey_y=vec[0].y;
+		while(i<vec.size()){
+			if(vec[i].distance<min){
+				prey_x=vec[i].x;
+				prey_y=vec[i].y;
+			}
+			i++;
+		}	
+		return true;
+	}
+	else{
+		return false;
+	}
+}
--- src/Alien.hpp
+++ src/Alien.hpp
@@ -6,7 +6,9 @@
 class Alien:public Carnivore{
 	public:
 	Alien(Universe& u,int x, int y,float currentAge);
-	void update();
+	void update_logic();
+	bool find(int & prey_x,int & prey_y,int predator_x,int predator_y);
+	void interact(Organism * O);
 	char ch() const ;
 	float speed() const ;
 	int power() const ;
