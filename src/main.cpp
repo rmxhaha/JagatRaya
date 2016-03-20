b1a5c3a37b2c9aea4e397065c31b69bae0e5668a
ab <alif_bhaskoro@yahoo.com>
2016-03-20 21:48:31 +0700

    driver buat test alien

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "Human.hpp"
 #include "windows.h"
 #include "Eagle.hpp"
+#include "Alien.hpp"
 #include "Tiger.hpp"
 #include "Grass.hpp"
 #include "Poison_Ivy.hpp"
@@ -16,11 +17,19 @@ int main()
     srand(444);
     Board b(10,10);
 	UniverseList u(b,2);
-//	Human* h = new Human(u,0,0,10);
-//	Tiger* t = new Tiger(u,5,5,10);
+	Alien* h = new Alien(u,0,0,10);
+	Human* h2 = new Human(u,5,5,10);
+	Human* h3 = new Human(u,3,3,10);
+	Human* h4 = new Human(u,2,8,10);
+	Human* h5 = new Human(u,7,6,10);
+	//Tiger* t = new Tiger(u,5,5,10);
 	Eagle *e = new Eagle(u,5,4,10);
- //   u.add(h);
-   // u.add(t);
+    u.add(h);
+    //u.add(t);
+    u.add(h2);
+    u.add(h3);
+    u.add(h4);
+    u.add(h5);
     u.add(e);
     while(true){
 		if(GetAsyncKeyState(VK_ESCAPE)){
