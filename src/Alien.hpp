2fdcac909dbfaba2f0ab90b7330359a7601f4014
rmxhaha <code000123@gmail.com>
2016-03-21 23:54:43 +0700

    Alien documentation added

--- src/Alien.hpp
+++ src/Alien.hpp
@@ -3,15 +3,29 @@
 #ifndef ALIEN_H
 #define ALIEN_H
 
+/**
+ * Alien class.
+ * Alien will kill everything in it's way. Alien have the highest speed.
+ */
 class Alien:public Carnivore{
 	public:
 	Alien(Universe& u,int x, int y,float currentAge);
 	void update_logic();
 	bool find(int & prey_x,int & prey_y,int predator_x,int predator_y);
-	void interact(Organism * O);
-	char ch() const ;
-	float speed() const ;
-	int power() const ;
-	int age() const ;
+    /** \brief see Organism::ch
+     */
+    char ch() const ;
+    /** \brief see Organism::age
+     */
+	int age() const;
+    /** \brief see Organism::power
+     */
+	int power() const;
+    /** \brief see Animal::speed
+     */
+    float speed() const;
+    /** \brief see Organism::interact
+     */
+    void interact(Organism*);
 };
 #endif
