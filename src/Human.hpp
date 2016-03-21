741879d684407ce9878f60196e13576d29e1b872
rmxhaha <code000123@gmail.com>
2016-03-21 23:03:40 +0700

    adding documentation

--- src/Human.hpp
+++ src/Human.hpp
@@ -2,14 +2,33 @@
 #define HUMAN_H
 
 #include "Carnivore.hpp"
-
+/**
+ * Human class.
+ * Human can copulate with other Human but has a limit in the number of offspring to prevent overpopulation.
+ */
 class Human : public Carnivore {
 public:
+    /** \brief Human Constructor
+     *
+     * \param universe Universe it belong in
+     * \return Human
+     *
+     */
     Human(Universe& universe, int x, int y,float currentAge);
-    virtual char ch() const ;
-	virtual int age() const;
-	virtual int power() const;
-    virtual float speed() const;
+    /** \brief see Organism::ch
+     */
+    char ch() const ;
+    /** \brief see Organism::age
+     */
+	int age() const;
+    /** \brief see Organism::power
+     */
+	int power() const;
+    /** \brief see Animal::speed
+     */
+    float speed() const;
+    /** \brief see Organism::interact
+     */
     void interact(Organism*);
 
 protected:
