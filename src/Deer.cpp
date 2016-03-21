b3633cb2b3fd948d9f5d2e553a68ff1c6a6edb3a
ab <alif_bhaskoro@yahoo.com>
2016-03-22 00:03:28 +0700

    Deer behaviour

--- src/Deer.cpp
+++ src/Deer.cpp
@@ -4,7 +4,19 @@ Deer::Deer(Universe& u, int x, int y, int t_lahir):Herbivore(u,x,y,t_lahir){}
 
 void Deer::update_logic(){
 	//Update Tiger move
-	move(goRandom());
+	int prey_x;
+	int prey_y;
+	if(findPrey('T',prey_x,prey_y,x,y)){
+		move(avoid(prey_x,prey_y));
+	}
+	else{
+		if(findPrey('D',prey_x,prey_y,x,y)){
+			move(goTo(prey_x,prey_y));
+		}
+		else{
+			move(goRandom());
+		}
+	}
 }
 char Deer::ch() const {
 	return 'D';
@@ -16,7 +28,7 @@ int Deer::power() const {
 	return 5;
 }
 int Deer::age() const {
-	return 60;
+	return 60000000;
 }
 
 void Deer::interact(Organism*){
