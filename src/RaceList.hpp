f037dfb238260a076af76f7271e13466d554ff8a
dendyliu <dendyliu@yahoo.com>
2016-03-22 15:33:06 +0700

    comment racelist

--- src/RaceList.hpp
+++ src/RaceList.hpp
@@ -1,9 +1,22 @@
 #include "Race.hpp"
 
+/**
+ * RaceNode class.
+ * RaceNode container node of Organism
+ */
 class RaceNode
 {
 public:
+    /** \brief RaceNode Constructor
+     *
+     * \param val Race to contain
+     * \return Race
+     *
+     */
     RaceNode(Race* val = NULL) : val(val), next(NULL) {}
+    /** \brief RaceNode Destructor
+     *
+     */
     ~RaceNode() {delete val;}
 
     Race* val;
