b15c2e3094f4912be5b6a96fb027d43830f8893b
rmxhaha <code000123@gmail.com>
2016-04-05 15:32:51 +0700

    Organism complete

--- jsrc/Organism.java
+++ jsrc/Organism.java
@@ -1,21 +1,39 @@
 
-class Organism {
+public abstract class Organism {
   protected float cAge;
   protected boolean is_dead;
   protected int x,y;
   //protected Universe universe;
 
+  public Organism(int x, int y, float currentAge){
+    this.x = x;
+    this.y = y;
+
+    cAge = currentAge;
+    is_dead = false;
+  }
   public int getX(){
     return x;
   }
   public int getY(){
     return y;
   }
-
-  Organism(int x, int y, float currentAge){
-    this.x = x;
-    this.y = y;
-    this.cAge = currentAge;
+  protected void setX(int _x){
+    x=_x;
+  }
+  protected void setY(int _y){
+    y=_y;
+  }
+  protected void setCurrentAge(float age){
+    cAge = age;
+  }
+  public abstract char ch();
+  public abstract int age();
+  public abstract int power();
+  public abstract void update(float dt);
+  public abstract void interact(Organism pair);
+  public void forceKill(){
+    is_dead = true;
   }
 
 };
